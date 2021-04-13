10181c462b14a1db7412c680ccc46794646b9f53
andrehen <andrehen@gmail.com>
2021-04-13 00:57:11 -0300

    Changing logic to accept files in any order. And to not loose any HR data or Location data.

--- gpxmaker.js
+++ gpxmaker.js
@@ -1,13 +1,3 @@
-
-const data_reduce = (arr) => {
-    return arr.reduce((obj, item, inn) => {
-        let start_time = Math.round(item.start_time / 1000);
-        //delete item.start_time;
-        obj[start_time] = item
-        return obj
-    }, {})
-}
-
 const save = (filename, data) => {
     var blob = new Blob([data], {type: 'text/xml'});
     if(window.navigator.msSaveOrOpenBlob) {
@@ -27,52 +17,59 @@ const isoData = (d) => (new Date(d)).toISOString();
 
 const handleFileSelect = (evt) => {
     const files = evt.target.files; // FileList object
-    const cnt = files.length;
+    const qtyFiles = files.length;
 
     // files is a FileList of File objects. List some properties.
     const output = [];
-    let merged = {};
+    let merged = [];
+
+    let filesProcessed = 0;
 
     if (typeof window.FileReader !== 'function') {
         document.getElementById('result').innerHTML = 'The file API isn\'t supported on this browser yet.';
         return;
     }
 
-    const finalize = (data) => {
+    if (qtyFiles != 2) {
+        document.getElementById('result').innerHTML = 'There should be 2 files. The location_data and the live_data';
+        return;
+    }
+
+    const finalize = data => {
         //console.log('final data:', data);
         let gpx = `<?xml version="1.0" encoding="UTF-8"?>
 <gpx creator="StravaGPX" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3">
     <metadata>
-        <time>${isoData(Object.keys(data)[0] * 1000)}</time>
+        <time>${isoData(data[0].start_time)}</time>
     </metadata>
     <trk>
         <name>My New Ride</name>
         <type>1</type>
         <trkseg>`;
 
-        Object.values(data).reduce(function(previousValue, currentValue, index, array){
-            //console.log('index', index, 'currentValue', currentValue);
-            if(currentValue.latitude && currentValue.longitude){
+        data.forEach(entry => {
+            if (!entry.latitude || !entry.longitude ) return;
+
+            gpx += `
+        <trkpt lat="${entry.latitude}" lon="${entry.longitude}">
+            <time>${isoData(entry.start_time)}</time>`;
+
+            if(entry.altitude){
                 gpx += `
-            <trkpt lat="${currentValue.latitude}" lon="${currentValue.longitude}">
-                <time>${isoData(currentValue.start_time)}</time>`;
-                if(currentValue.altitude){
-                    gpx += `
-                <ele>${currentValue.altitude}</ele>`;
-                }
-                if(previousValue.heart_rate){
-                    gpx += `
-                <extensions>
-                    <gpxtpx:TrackPointExtension>
-                        <gpxtpx:hr>${previousValue.heart_rate}</gpxtpx:hr>
-                    </gpxtpx:TrackPointExtension>
-                </extensions>`;
-                }
-                gpx += `
-            </trkpt>`;
+            <ele>${entry.altitude}</ele>`;
             }
-            return currentValue;
-        })
+
+            if (entry.heart_rate) {
+                gpx += `
+            <extensions>
+                <gpxtpx:TrackPointExtension>
+                    <gpxtpx:hr>${entry.heart_rate}</gpxtpx:hr>
+                </gpxtpx:TrackPointExtension>
+            </extensions>`;
+            }
+            gpx += `
+        </trkpt>`;
+        });
 
         gpx += `
         </trkseg>
@@ -99,23 +96,46 @@ const handleFileSelect = (evt) => {
         fr.onload = function(e) {
             let result = JSON.parse(e.target.result);
 
-            let formatted_result = data_reduce(result);
+            // If we are on the live_data data, we filter to only have heart_rate data.
+            if (result.find(e => e.heart_rate != null)) {
+                result = result.filter(e => e.heart_rate!= null );
+            }
 
-            //console.log('result', result);
-            //console.log('formatted_result', formatted_result);
+            merged = [...merged, ...result];
 
-            Object.assign(merged, formatted_result);
+            filesProcessed++;
+            if(filesProcessed === qtyFiles){
+                // Sort by time
+                merged.sort((a, b) => {
+                    if (a.start_time > b.start_time) return 1;
+                    if (a.start_time < b.start_time) return -1;
+                    return 0;
+                })
 
-            if(cnt === idx + 1){
-                finalize(merged);
+                let storedEntry = null;
+                let consolidatedData = merged.map((entry, index, orignal) => {
+                    if (entry.heart_rate != null) {
+                        storedEntry = entry;
+                        return null;
+                    }
+
+                    if (storedEntry != null) {
+                        entry.heart_rate = storedEntry.heart_rate;
+                        storedEntry = null;
+                        return entry
+                    }
+
+                    return entry;
+                });
+
+                consolidatedData = consolidatedData.filter(e => e != null);
+
+                finalize(consolidatedData);
             }
         }
         fr.readAsText(f);
 
     });
     
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-
-    //console.log('merged', merged);
-    
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';    
 }
